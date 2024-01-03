63f167b991907a21befc8036bf90e0fc6a0c236b
Jasonzzt <ziteng.zhang@intel.com>
2024-01-03 23:03:54 +0800

    init

---
+++ lab1v7/test/bin/pc/test.bat
@@ -0,0 +1,13 @@
+call r1_httpd.bat
+
+call r2_rmid.bat
+
+call r3_reggie.bat
+
+start cmd /k call chatserver.bat
+
+start cmd /k call chatclient.bat
+
+start cmd /k call chatclient.bat
+
+cmd /k
---
+++ lab2v13/test/bin/pc/test.bat
@@ -0,0 +1,19 @@
+call r1_httpd.bat
+
+call r2_rmid.bat
+
+call r3_reggie.bat
+
+start cmd /k call Bailiff.bat -id 111
+
+start cmd /k call Bailiff.bat -id 222
+
+start cmd /k call Bailiff.bat -id 333
+
+call Dexter.bat -id Zhang -t
+
+call Dexter.bat -id fki  
+
+call Dexter.bat -id Ma
+
+cmd /k
